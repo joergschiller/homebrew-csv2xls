f3b027a8deb3c80c195338358974c406d1a7b850
Jörg Schiller <joergschiller@googlemail.com>
2016-04-07 15:53:15 +0200

    Fix brew audit errors and warnings.

--- csv2xls.rb
+++ csv2xls.rb
@@ -1,17 +1,19 @@
-require "formula"
-
 class Csv2xls < Formula
-  homepage 'http://ferkulat.github.io/csv2xls/'
-  url 'https://github.com/ferkulat/csv2xls/releases/download/0.3.4/csv2xls-0.3.4.tar.bz2'
-  sha1 "6140301b3187932c46fc02ea7bf70f2b0be6421b"
+  desc "command-line utility which converts csv files into Excel(TM) files"
+  homepage "https://ferkulat.github.io/csv2xls/"
+  url "https://github.com/ferkulat/csv2xls/releases/download/0.3.4/csv2xls-0.3.4.tar.bz2"
+  sha256 "b14fb863bb9305eb11b9ba8b18001016d8a029af2715bb5e61f53c844923e10c"
 
-  depends_on 'xlslib'
-  depends_on 'libcsv'
+  depends_on "xlslib"
+  depends_on "libcsv"
 
   def install
-
     system "./configure", "--disable-debug"
     system "make"
     system "make", "install"
+  end
+
+  test do
+    system "#{bin}/csv2xls", "--version"
   end
 end
