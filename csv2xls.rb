013bf5903f6ee0c3b698b6be7bb8e49ab093bf38
Jörg Schiller <joergschiller@googlemail.com>
2016-04-07 16:33:45 +0200

    Bump to 0.3.5.

--- csv2xls.rb
+++ csv2xls.rb
@@ -1,8 +1,8 @@
 class Csv2xls < Formula
   desc "command-line utility which converts csv files into Excel(TM) files"
   homepage "https://ferkulat.github.io/csv2xls/"
-  url "https://github.com/ferkulat/csv2xls/releases/download/0.3.4/csv2xls-0.3.4.tar.bz2"
-  sha256 "b14fb863bb9305eb11b9ba8b18001016d8a029af2715bb5e61f53c844923e10c"
+  url "https://github.com/ferkulat/csv2xls/releases/download/0.3.5/csv2xls-0.3.5.tar.bz2"
+  sha256 "580a1d39b5d37bfa1b63ff5b1c9d4008243a738e416f417c8dbaaffa83079c45"
 
   depends_on "xlslib"
   depends_on "libcsv"
