ca6e78d997f0b9b6d6b665524b826dffa4a84bf9
MrPans <shengpann@163.com>
2024-05-08 17:33:44 +0800

    fix download url
    fix download url

--- csv2xls.rb
+++ csv2xls.rb
@@ -1,7 +1,7 @@
 class Csv2xls < Formula
   desc "command-line utility which converts csv files into Excel(TM) files"
   homepage "https://ferkulat.github.io/csv2xls/"
-  url "https://github.com/ferkulat/csv2xls/releases/download/0.3.5/csv2xls-0.3.5.tar.bz2"
+  url "https://github.com/ferkulat/csv2xls/releases/download/v0.3.5/csv2xls-0.3.5.tar.bz2"
   sha256 "580a1d39b5d37bfa1b63ff5b1c9d4008243a738e416f417c8dbaaffa83079c45"
 
   depends_on "xlslib"
